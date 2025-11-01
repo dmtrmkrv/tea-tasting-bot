a78a8b33306d8d603b7990bc9ded248a67772916
dmtrmkrv <43113473+dmtrmkrv@users.noreply.github.com>
2025-11-01 18:11:14 +0300

    Add empty __init__ for app package (#12)
    * Add empty __init__ for app package
    
    * Ensure alembic env adds project root to path

--- alembic/env.py
+++ alembic/env.py
@@ -1,4 +1,9 @@
-import os
+import os, sys
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
+
 from urllib.parse import quote_plus
 from logging.config import fileConfig
 from sqlalchemy import engine_from_config, pool
